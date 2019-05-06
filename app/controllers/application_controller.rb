f8faa1be55831faef275c5de5446599991e652e4
Katherine Nguyen <khnguyen86@live.com>
2019-05-05 20:03:41 -0700

    adds after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  # Overrides default devise after sign in path
+  def after_sign_in_path_for(user)
+    dashboard_path
+  end
 end
