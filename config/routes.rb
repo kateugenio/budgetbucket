4a99cb7da949ac18c146e4faa8e0da27d23e1c56
Katherine Nguyen <khnguyen86@live.com>
2019-05-05 20:04:24 -0700

    initializes devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords' }
+  as :user do
+    get '/', to: 'users/sessions#new'
+  end
 
-  root 'dashboard#dashboard'
+  get 'dashboard', to: 'dashboard#dashboard'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
