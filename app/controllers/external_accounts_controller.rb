51e7586dbbf1959fe3382962edf18d9a584875ff
Katherine Nguyen <khnguyen86@live.com>
2019-11-14 21:30:02 -0800

    adds metadata and create_from_service actions, creates methods to fetch access token from plaid and cache account and token results

--- app/controllers/external_accounts_controller.rb
+++ app/controllers/external_accounts_controller.rb
@@ -4,16 +4,47 @@ class ExternalAccountsController < ApplicationController
   # POST /metadata
   def metadata
     respond_to do |format|
-      @public_token = params[:public_token]
-      @metadata = params[:metadata]
+      @account = @user.accounts.new
+      public_token = params[:public_token]
+      @institution_name = params[:metadata][:institution][:name]
+      depository_accounts = params[:metadata][:accounts].select { |key, account| account[:type] == 'depository' }
+      @accounts = []
+      depository_accounts.each_pair do |_key, account|
+        @accounts << {
+          id: account['id'],
+          name: account['name'],
+          mask: account['mask'],
+          subtype: account['subtype']
+        }
+      end
+
+      fetch_plaid_access_token_and_cache_token_and_accounts(public_token, @accounts)
 
       format.js
     end
+  end
+
+  def create_from_service
+    puts params.inspect
   end
 
   private
 
   def set_user
     @user = current_user
+  end
+
+  def fetch_plaid_access_token_and_cache_token_and_accounts(public_token, accounts)
+    plaid_service = PlaidService.new
+    response = plaid_service.token_exchange(public_token)
+    cache_metadata(response[:access_token], response[:item_id], accounts)
+  end
+
+  def cache_metadata(access_token, item_id, accounts)
+    Rails.cache.fetch("/users/#{@user.id}/external_accounts") do
+      { access_token: access_token, accounts: accounts }
+    end
+
+    cache = Rails.cache.fetch("/users/#{@user.id}/external_accounts")
   end
 end
