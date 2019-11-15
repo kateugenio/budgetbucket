41ab8279296031da1e0704ef761d215e4d221e28
Katherine Nguyen <khnguyen86@live.com>
2019-11-14 21:31:43 -0800

    adds routes to create account from service

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
     get '/', to: 'users/sessions#new'
   end
 
+  # DashboardController
   get 'dashboard', to: 'dashboard#dashboard'
 
+  # ExternalAccountsController
   post 'metadata', to: 'external_accounts#metadata'
+  post 'create_from_service', to: 'external_accounts#create_from_service'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
