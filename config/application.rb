e1ba9bfa1682176b57a46b3d5670910cc23cdf15
Katherine Nguyen <khnguyen86@live.com>
2019-12-01 13:38:39 -0800

    adds argon design system and updates config for asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Budgetbucket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
+
+    config.assets.paths << Rails.root.join('vendor', 'argon-dashboard-master')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
