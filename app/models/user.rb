c284e7f8863e3d641360484c610259f0d19a8a94
Katherine Nguyen <khnguyen86@live.com>
2020-01-08 22:16:10 -0800

    adds validation that new users are part of authorized-staging-users list from google sheet

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :accounts, dependent: :destroy
+
+  validate :authorized_staging_user?
+
+  def authorized_staging_user?
+    return unless Rails.env.staging?
+
+    google = GoogleDriveService.new
+    google.authorized_staging_user?(email)
+
+    errors[:base] << "Sorry, registration was not successful. You must be an invited user."
+  end
 end
